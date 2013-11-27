9b8da06bdffee303161a132beb1861fa592b5e13
Tomasz Boczkowski <tomasz.boczkowski@onet.pl>
2013-11-27 21:34:21 +0100

    Moved tests to uitests

---
+++ pl.poznan.put.cs.gui4pddl/tests/pl/poznan/put/cs/gui4pddl/tests/manual/ParsePDDL.java
@@ -0,0 +1,76 @@
+package pl.poznan.put.cs.gui4pddl.tests.manual;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream.GetField;
+
+import org.antlr.runtime.ANTLRInputStream;
+import org.antlr.runtime.CommonTokenStream;
+import org.antlr.runtime.RecognitionException;
+import org.antlr.runtime.tree.CommonTree;
+import org.antlr.runtime.tree.CommonTreeNodeStream;
+
+import pl.poznan.put.cs.gui4pddl.PDDLStructureConfigHelpers;
+import pl.poznan.put.cs.gui4pddl.codemodel.IPDDLCodeModel;
+import pl.poznan.put.cs.gui4pddl.codemodel.PDDLCodeModel;
+import pl.poznan.put.cs.gui4pddl.codemodel.PDDLCodeOutline;
+import pl.poznan.put.cs.gui4pddl.codemodel.PDDLFile;
+import pl.poznan.put.cs.gui4pddl.parser.PDDLLexer;
+import pl.poznan.put.cs.gui4pddl.parser.PDDLModelBuilder;
+import pl.poznan.put.cs.gui4pddl.parser.PDDLParser;
+
+
+
+public class ParsePDDL {
+
+	void run() {
+
+		PDDLCodeModel model = new PDDLCodeModel();
+		
+		InputStream stream;
+		try {
+			stream = this.getClass().getResourceAsStream("test.pddl");
+			
+			
+			//stream = new FileInputStream("test.pddl");
+			ANTLRInputStream input = new ANTLRInputStream(stream);
+			PDDLLexer lexer = new PDDLLexer(input);
+			CommonTokenStream tokens = new CommonTokenStream(lexer);
+			PDDLParser parser = new PDDLParser(tokens);
+			
+			try {
+				PDDLParser.pddl_file_return ret = parser.pddl_file();
+				CommonTree t = (CommonTree)ret.getTree();
+				System.out.println(t.toStringTree());
+				
+				 CommonTreeNodeStream nodes = new CommonTreeNodeStream(t);
+			     PDDLModelBuilder walker = new PDDLModelBuilder(nodes); // create a tree parser
+			     
+			     
+			     PDDLFile file = model.getOrCreateFile("test.pddl");
+			     walker.pddl_file(file);                 // launch at start rule prog
+			     
+			     PDDLCodeOutline outline = new PDDLCodeOutline();
+			     String text = outline.getOutline(model);
+			     System.out.println(text);
+			
+			} catch (RecognitionException e) {
+				e.printStackTrace();
+			}
+		
+		
+		} catch (FileNotFoundException e) {
+			System.out.println(e.getMessage());
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	public static void main(String[] args) {
+		ParsePDDL runner = new ParsePDDL();
+		runner.run();
+	}
+
+}
---
+++ pl.poznan.put.cs.gui4pddl/tests/pl/poznan/put/cs/gui4pddl/tests/manual/test.pddl
@@ -0,0 +1,14 @@
+(define
+	(domain world-of-blocks)
+	(:predicates 
+		(on-top ?x ?y)
+		(on-floor ?x)
+		(clear ?x)
+	)
+	(:requirements :adl)
+	(:extends sasd)
+	(:requirements :adl :d)
+	(:types a b d sdfa sfad - wwwh sdfuh - ( fluent  asdf ) dgs dfsgh)
+; przesuń klocek na podłogę
+        
+)
